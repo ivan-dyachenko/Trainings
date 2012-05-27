d075023442f81216c552114e4e3d853510740161
Ivan Dyachenko <ivan.dyachenko@gmail.com>
2012-05-27 19:54:36 +0300

    added two tests

--- Test Driven Development/sources/tdd-bowling-kata/java/src/main/java/kata/BowlingGame.java
+++ Test Driven Development/sources/tdd-bowling-kata/java/src/main/java/kata/BowlingGame.java
@@ -2,4 +2,13 @@ package kata;
 
 public class BowlingGame {
 
+    private int score;
+
+    public void roll(int pins) {
+        score += pins;
+    }
+
+    public int score() {
+        return score;
+    }
 }
--- Test Driven Development/sources/tdd-bowling-kata/java/src/test/java/kata/BowlingGameTest.java
+++ Test Driven Development/sources/tdd-bowling-kata/java/src/test/java/kata/BowlingGameTest.java
@@ -1,13 +1,41 @@
 package kata;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
 
 public class BowlingGameTest {
+
+    /**
+     * should score zero on gutter game
+     * should score twenty when all one
+     * should score spare correctly
+     * should score all spares correctly
+     * should score strike correctly
+     * should score super game
+     */
 
     private BowlingGame game;
 
     @Before
     public void setUp() throws Exception {
         game = new BowlingGame();
+    }
+
+    @Test
+    public void shouldScoreZeroOnGutterGame() {
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    public void shouldScoreTwentyWhenAllOne() {
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertEquals(20, game.score());
     }
 }
