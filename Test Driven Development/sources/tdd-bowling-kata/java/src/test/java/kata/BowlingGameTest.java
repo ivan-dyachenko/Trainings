28b8b2c3477ddd9b252ee650f5cbdf9235d12ffa
Ivan Dyachenko <ivan.dyachenko@gmail.com>
2012-05-27 18:14:33 +0300

    add BowlingGameTest.java with setUp JUnit 4.0

---
+++ Test Driven Development/sources/tdd-bowling-kata/java/src/test/java/kata/BowlingGameTest.java
@@ -0,0 +1,13 @@
+package kata;
+
+import org.junit.Before;
+
+public class BowlingGameTest {
+
+    private BowlingGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGame();
+    }
+}
